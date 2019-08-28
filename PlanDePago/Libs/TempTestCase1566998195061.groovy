30b9991e7dc4b2a35cfc525a8f49b3a2809ad536
Mariano Laje <marianolaje@outlook.com>
2019-08-28 12:45:45 -0300

    version 1.0

---
+++ PlanDePago/Libs/TempTestCase1566998195061.groovy
@@ -0,0 +1,22 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+import com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('C:\\Users\\maria\\AppData\\Local\\Temp\\Katalon\\Test Cases\\WSPlanDePago\\obo plan de pago\\20190828_101635\\execution.properties')
+
+TestCaseMain.beforeStart()
+
+        TestCaseMain.runTestCase('Test Cases/WSPlanDePago/obo plan de pago', new TestCaseBinding('Test Cases/WSPlanDePago/obo plan de pago',[:]), FailureHandling.STOP_ON_FAILURE , false)
+    
